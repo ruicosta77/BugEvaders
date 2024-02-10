f7535fae3713f2f123ff496ba3053cf663196eff
Igor Gomes Leandro <131786409+igorgleandro@users.noreply.github.com>
2024-02-10 15:10:56 +0000

    Add files via upload

---
+++ BugEvaders/src/io/codeforall/bootcamp/filhosdamain/Main.java
@@ -0,0 +1,10 @@
+
+package io.codeforall.bootcamp.filhosdamain;
+
+public class Main {
+
+    public static void main(String[] args) {
+       MyLittleObject myLittleObject = new MyLittleObject();
+       MyLittleControls myLittleControls = new MyLittleControls(myLittleObject);
+    }
+}
